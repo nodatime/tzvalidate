9b7b341939d3c2b95f7e7f11ef0cb9d57b54eb70
Jon Skeet <skeet@pobox.com>
2015-07-14 19:33:20 +0100

    Fix for the Ruby line ending issue.

--- ruby/tzdump.rb
+++ ruby/tzdump.rb
@@ -1,6 +1,3 @@
-# Note: this currently uses the system line break, instead of specifying CR-LF.
-# I'm not sure how to specify the line break when running Ruby...
-
 require 'tzinfo'
 
 # Constants
@@ -14,29 +11,29 @@ def format_offset(offset)
   return '%s%02d:%02d:%02d' % [sign, offset / 3600, (offset / 60) % 60, offset % 60]
 end
 
-def dump_zone(id)
-  puts "#{id}"
+def dump_zone(console, id)
+  console.write "#{id}\r\n"
   zone = TZInfo::Timezone.get(id)
   period = zone.period_for_utc(START_UTC)
   if (period.end_transition.nil? || period.end_transition.at > END_UTC)
-    puts "Fixed: #{format_offset(period.offset.utc_total_offset)} #{period.offset.abbreviation}"
+    console.write "Fixed: #{format_offset(period.offset.utc_total_offset)} #{period.offset.abbreviation}\r\n"
   else
     while !period.end_transition.nil? && period.end_transition.at < END_UTC
       period = zone.period_for_utc(period.end_transition.at)
       start = period.start_transition.at.to_datetime
       formatted_start = start.strftime("%Y-%m-%dT%H:%M:%SZ")
       formatted_offset = format_offset(period.offset.utc_total_offset)
-      puts "#{formatted_start} #{formatted_offset} #{period.dst? ? "daylight" : "standard"} #{period.offset.abbreviation}"
+      console.write "#{formatted_start} #{formatted_offset} #{period.dst? ? "daylight" : "standard"} #{period.offset.abbreviation}\r\n"
     end
   end
 end
 
-
+console = IO.new(STDOUT.fileno, mode: 'w', universal_newline: true)
 if ARGV.empty?
   for id in TZInfo::Timezone.all_identifiers.sort
-    dump_zone(id)
-    puts()
+    dump_zone(console, id)
+    console.write "\r\n"
   end
 else
-  dump_zone(ARGV[0])
+  dump_zone(console, ARGV[0])
 end
