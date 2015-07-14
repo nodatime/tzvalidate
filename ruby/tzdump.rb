b33bfd57f2b946e6fd9726104c744e17fc56942e
Will <will+dev@kiwinet.org>
2015-07-15 09:04:15 +1200

    `$stdout` should work as-well as copying the STDOUT FH into a new IO object

--- ruby/tzdump.rb
+++ ruby/tzdump.rb
@@ -13,19 +13,19 @@ def format_offset(offset)
   return '%s%02d:%02d:%02d' % [sign, offset / 3600, (offset / 60) % 60, offset % 60]
 end
 
-def dump_zone(console, id)
-  console.write "#{id}\r\n"
+def dump_zone(id)
+  $stdout.write "#{id}\r\n"
   zone = TZInfo::Timezone.get(id)
   period = zone.period_for_utc(START_UTC)
   if (period.end_transition.nil? || period.end_transition.at > END_UTC)
-    console.write "Fixed: #{format_offset(period.offset.utc_total_offset)} #{period.offset.abbreviation}\r\n"
+    $stdout.write "Fixed: #{format_offset(period.offset.utc_total_offset)} #{period.offset.abbreviation}\r\n"
   else
     while !period.end_transition.nil? && period.end_transition.at < END_UTC
       period = zone.period_for_utc(period.end_transition.at)
       start = period.start_transition.at.to_datetime
       formatted_start = start.strftime("%Y-%m-%dT%H:%M:%SZ")
       formatted_offset = format_offset(period.offset.utc_total_offset)
-      console.write "#{formatted_start} #{formatted_offset} #{period.dst? ? "daylight" : "standard"} #{period.offset.abbreviation}\r\n"
+      $stdout.write "#{formatted_start} #{formatted_offset} #{period.dst? ? "daylight" : "standard"} #{period.offset.abbreviation}\r\n"
     end
   end
 end
@@ -37,8 +37,7 @@ tzs = if ARGV.empty?
         ARGV[0]
       end
 
-console = IO.new(STDOUT.fileno, mode: 'w', universal_newline: true)
 tzs.each do |id|
-  dump_zone(console, id)
-  console.write "\r\n"
+  dump_zone(id)
+  $stdout.write "\r\n"
 end
