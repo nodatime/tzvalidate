43f2270055883e7f90f0fe9b63c673c102c7ab57
Jon Skeet <skeet@pobox.com>
2016-05-06 15:59:55 +0100

    Update Java code to new format, and make it a small framework

--- java/org/nodatime/tzvalidate/Java8Dump.java
+++ java/org/nodatime/tzvalidate/Java8Dump.java
@@ -5,6 +5,7 @@
 package org.nodatime.tzvalidate;
 
 import java.io.IOException;
+import java.io.Writer;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
@@ -12,58 +13,35 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.zone.ZoneOffsetTransition;
 import java.time.zone.ZoneRules;
-import java.time.zone.ZoneRulesProvider;
 import java.util.Locale;
-import java.util.Set;
-import java.util.TreeSet;
 
 import org.apache.commons.cli.ParseException;
 
-public final class Java8Dump {
+public final class Java8Dump implements ZoneDumper {
     private static final DateTimeFormatter INSTANT_FORMAT = DateTimeFormatter
-        .ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US)
+        .ofPattern("yyyy-MM-dd HH:mm:ss'Z'", Locale.US)
         .withZone(ZoneOffset.UTC);
     private static final DateTimeFormatter ZONE_NAME_FORMAT = DateTimeFormatter.ofPattern("zzz", Locale.US);
 
     public static void main(String[] args) throws IOException, ParseException {
-        DumpOptions options = DumpOptions.parse("Java8Dump", args, true);
-        if (options == null) {
-            return;
-        }
-        
-        String zoneId = options.getZoneId();
-        if (zoneId != null) {
-            ZoneId zone = ZoneId.of(zoneId);
-            if (zone == null) {
-                System.out.println("Zone " + zoneId + " does not exist");
-            } else {
-                dumpZone(zone, options);
-            }
-        } else {
-            // Sort IDs in ordinal fashion
-            Set<String> ids = new TreeSet<>(ZoneRulesProvider.getAvailableZoneIds());
-            for (String id : ids) {
-                dumpZone(ZoneId.of(id), options);
-                System.out.printf("\r\n");
-            }
-        }
+        DumpCoordinator.dump(new Java8Dump(), false, args);
     }
 
-    private static void dumpZone(ZoneId zone, DumpOptions options) {
-        System.out.printf("%s\r\n", zone.getId());
+    public void dumpZone(String id, int fromYear, int toYear, Writer writer) throws IOException {
+        writer.write(id + "\n");
+        ZoneId zone = ZoneId.of(id);
         // Instant.MIN can't be formatted, so let's just do "quite a long time ago"
         Instant early = ZonedDateTime.of(1, 1, 1, 0, 0, 0, 0, ZoneOffset.UTC).toInstant();
-        System.out.printf("Initially:           %s\r\n", formatOffsetAndName(zone, early));
+        writer.write("Initially:           " + formatOffsetAndName(zone, early) + "\n");  
 
-        Instant start = ZonedDateTime.of(options.getFromYear(), 1, 1, 0, 0, 0, 0, ZoneOffset.UTC).toInstant();
-        Instant end = ZonedDateTime.of(options.getToYear(), 1, 1, 0, 0, 0, 0, ZoneOffset.UTC).toInstant();
+        Instant start = ZonedDateTime.of(fromYear, 1, 1, 0, 0, 0, 0, ZoneOffset.UTC).toInstant();
+        Instant end = ZonedDateTime.of(toYear, 1, 1, 0, 0, 0, 0, ZoneOffset.UTC).toInstant();
         
         ZoneRules rules = zone.getRules();
         ZoneOffsetTransition transition = rules.nextTransition(start.minusNanos(1));
         while (transition != null && transition.getInstant().isBefore(end)) {
-            System.out.printf("%s %s\r\n",
-                    INSTANT_FORMAT.format(transition.getInstant()),
-                    formatOffsetAndName(zone, transition.getInstant()));
+            writer.write(INSTANT_FORMAT.format(transition.getInstant()) + " "
+                + formatOffsetAndName(zone, transition.getInstant()) + "\n");
             transition = rules.nextTransition(transition.getInstant());
         }
     }
@@ -80,5 +58,15 @@ public final class Java8Dump {
             sign, seconds / 3600, (seconds / 60) % 60, seconds % 60,
             rules.getDaylightSavings(instant).isZero() ? "standard" : "daylight",
             ZONE_NAME_FORMAT.format(instant.atZone(zone)));
+    }
+
+    @Override
+    public void initialize(DumpOptions options) {
+        // No-op
+    }
+
+    @Override
+    public Iterable<String> getZoneIds() {
+        return ZoneId.getAvailableZoneIds();
     }
 }
